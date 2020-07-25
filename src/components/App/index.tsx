a8e16b075b133d76026cd7baa5098a936db93b15
Gravewalker666 <heshanandrews99@gmail.com>
2020-07-25 20:47:33 +0530

    Add background image and finalize

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import {Avatar, Button} from "antd";
+import { Avatar } from "antd";
 import {SignInWithGoogle} from "../../service/firebase/firebase.utils";
 import firebase from "firebase";
 
@@ -26,29 +26,29 @@ class App extends React.Component<any, any> {
 
     render() {
         return (
-            <div className="main-content">
-                <div className="content">
-                    {this.state.isLoggedIn?
-                        (
-                            <div>
-                                <div onClick={ SignInWithGoogle }>
-                                    <Avatar size={100} src={this.state.currentUser.photoURL} />
-                                </div>
-                                <h2 className="text">{this.state.currentUser.displayName}</h2>
-                                <Button type="primary" onClick={ this.signOut }>I wanna get the hell outta this lava tub</Button>
+            <div>
+                {this.state.isLoggedIn?
+                    (
+                        <div className="main-content-logged-in">
+                            <div className="content">
+                                <Avatar className="profile-pic" size={100} src={this.state.currentUser.photoURL} />
+                                <h2 className="text">{this.state.currentUser.displayName} is in sweet hot lava</h2>
+                                <span className="logout-text" onClick={ this.signOut }>I wanna get the hell outta this lava tub</span>
                             </div>
-                        )
-                        : (
-                            <div>
+                        </div>
+                    )
+                    : (
+                        <div className="main-content">
+                            <div className="content">
                                 <span className="text">Sign in with</span><br/>
                                 <div onClick={ SignInWithGoogle }>
                                     <Avatar className="google-avatar" size={100} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" />
                                 </div>
                                 <span className="text">to get access to our <br/> new hot lava tub</span><br/>
                             </div>
-                        )
-                    }
-                </div>
+                        </div>
+                    )
+                }
             </div>
         );
     }
